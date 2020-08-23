938446a94afecbbc21e62bc6e03ed9004cd5a16d
unknown <cortinabryan@gmail.com>
2020-08-23 12:50:48 -0700

    finish menu and hamburger icon w/ functionality

---
+++ js/app.js
@@ -0,0 +1,23 @@
+const navTriggerEl = document.querySelector(".hamburger");
+const navEl = document.querySelector("nav");
+const containerEl = document.querySelector(".container");
+const hamburgerBarsEl = document.getElementsByTagName("span");
+
+console.log(navTriggerEl)
+
+function toggleNav(){
+    navTriggerEl.addEventListener("click", function() {
+        navEl.classList.toggle("open");
+        containerEl.classList.toggle("shift");
+        animateHamburger();
+    })
+}
+
+function animateHamburger(){
+    for (let span of hamburgerBarsEl){
+        span.classList.toggle("change")
+    }
+}
+
+
+toggleNav();
